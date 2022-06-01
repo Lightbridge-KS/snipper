88c4390818ca7f0ed89ff034443493e2ff458de5
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-06-01 22:40:21 +0700

    define global vars

--- R/snipper-package.R
+++ R/snipper-package.R
@@ -5,3 +5,11 @@
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
+
+# Declare global variables
+if(getRversion() >= "2.15.1") {
+
+  utils::globalVariables(c("description", "is_body_dubs", "prefix", "scope", "snippet_name"))
+
+}
+
